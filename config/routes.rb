13d6bc551df8cdc0937276b0dce7cb2987a65f81
Kyle <kjfossman@gmail.com>
2021-04-07 15:06:55 -0800

    commit tree 27b3ad6ffcd2b27638a48618c88134d8e5b5a928

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :students, only: [:index, :show, :activate]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, only: [:index, :show]
+  get "students/:id/activate", to: "students#activate", as: "activate_student"
 end
